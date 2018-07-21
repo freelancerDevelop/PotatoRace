99fb272fc938b24fea35b4e6e783a5666d4f4bd6
wikinto <wikintopl@gmail.com>
2018-07-22 00:31:51 +0200

    Trees and skybox

---
+++ PotatoRace/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    GameObject ply;
+    GameObject potat;
+	// Use this for initialization
+	void Start () {
+        ply = GameObject.Find("ThirdPersonController");
+        potat = GameObject.Find("RollerBall");
+        Camera cam = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector3 middle = (ply.transform.position - potat.transform.position) / 2f;
+        float dist = (ply.transform.position - potat.transform.position).magnitude;
+        Vector3 camdist = middle - this.transform.forward * dist * 2;
+        //this.transform.position = new Vector3(camdist.x/4f, ply.transform.position.y + 50, ply.transform.position.z -50);
+    }
+}
---
+++ PotatoRace/Assets/Scripts/FallingTrees.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTrees : MonoBehaviour {
+    int test;
+    void Start () {
+		
+	}
+	//
+	void Update () {
+		
+	}
+    IEnumerator OnTriggerEnter(Collider tree)
+    {
+        if(tree.tag == "Tree")
+        {
+            tree.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX;
+            tree.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+            tree.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 100);
+            yield return new WaitForSeconds(2);
+            tree.GetComponent<BoxCollider>().enabled = false;
+            tree.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+}
