a84a94d252b3dccf22271f29852e410180410ae8
Vasko <Eldrin>
2018-07-22 11:53:48 +0200

    Main menu

---
+++ PotatoRace/Assets/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button btn1 = this.GetComponent<Button>();
+       
+
+        //Calls the TaskOnClick/TaskWithParameters method when you click the Button
+        btn1.onClick.AddListener(TaskOnClick);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 void TaskOnClick()
+    {
+       SceneManager.LoadScene("SampleScene2");
+    }
+}
