d970d3772945405adb43a68da24f6e8adb39a39a
wikinto <wikintopl@gmail.com>
2018-07-22 09:44:24 +0200

    Change potato to unicorn script:

---
+++ PotatoRace/Assets/Scripts/Potat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potat : MonoBehaviour {
+    public GameObject unipotat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider ferty)
+    {
+        if(ferty.tag == "Fertyliser")
+        {
+            GameObject.Destroy(ferty.gameObject);
+            unipotat.SetActive(true);
+            unipotat.transform.position = this.transform.position;
+            unipotat.transform.rotation = this.transform.rotation;
+            Destroy(GameObject.FindGameObjectWithTag("Potato"));
+            
+
+        }
+    }
+}
